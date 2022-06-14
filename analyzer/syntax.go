cfc75cc9548d991245a51c25fafaf81e587fe082
Victor Lopes <victor.lopesms@gmail.com>
2022-06-14 00:40:51 -0300

    var statement recognition

--- analyzer/syntax.go
+++ analyzer/syntax.go
@@ -7,7 +7,7 @@ var currentToken = ""
 
 // Constructor
 func Syntax() {
-	tokens := []string{"program", "teste", ";"}
+	tokens := []string{"program", "teste", ";", "var", "teste", "{", "TDC"}
 	start(tokens)
 }
 
@@ -20,12 +20,12 @@ func nextToken(tokens []string) {
 
 // <Start> ::= 'program' Identifier ';' <GlobalStatement>
 func start(tokens []string) {
-	nextToken(tokens)
+	nextToken(tokens) // program
 	if currentToken == "program" {
 		nextToken(tokens) // identifies
-		nextToken(tokens)
-
+		nextToken(tokens) // ;
 		if currentToken == ";" {
+			nextToken(tokens) // var
 			globalStatement(tokens)
 		}
 	} else {
@@ -35,7 +35,6 @@ func start(tokens []string) {
 
 // <GlobalStatement> ::= <VarStatement> <ConstStatement> <RegisterStatement><ProcedureStatement><FunctionStatement> <Main>
 func globalStatement(tokens []string) {
-	nextToken(tokens)
 	varStatement(tokens)
 	// constStatement(tokens)
 	// registerStatement(tokens)
@@ -47,10 +46,70 @@ func globalStatement(tokens []string) {
 // <VarStatement>::= 'var' '{' <VarList>
 func varStatement(tokens []string) {
 	if currentToken == "var" {
-		nextToken(tokens)
+		nextToken(tokens) // {
 		if currentToken == "{" {
-			fmt.Print("Var")
-			// varList()
+			nextToken(tokens) // integer | string | real | boolean | char | identifier
+			varList(tokens)
 		}
+	} else {
+		// error
+	}
+}
+
+// <VarList>::= <VarDeclaration> <VarList> | '}'
+func varList(tokens []string) {
+	if currentToken == "}" {
+		nextToken(tokens)
+	} else {
+		varDeclaration(tokens)
+		varList(tokens)
+	}
+}
+
+// <VarDeclaration>::= <VarType> Identifier <VarDeclaration1>
+func varDeclaration(tokens []string) {
+	varType(tokens)
+	nextToken(tokens) // Identifier
+
+	// todo: Identifier
+
+	nextToken(tokens) // ,
+	varDeclaration1(tokens)
+}
+
+// <VarDeclaration1>::= ',' Identifier <VarDeclaration1> | ';'
+func varDeclaration1(tokens []string) {
+	if currentToken == "," {
+		nextToken(tokens)
+
+		// todo: Identifier
+
+		varDeclaration1(tokens)
+	} else if currentToken == ";" {
+		nextToken(tokens)
+	} else {
+		// error
+	}
+}
+
+// <VarType>::= 'integer' | 'string' | 'real' | 'boolean' | 'char' | Identifier
+func varType(tokens []string) {
+	if currentToken == "integer" {
+		fmt.Print("integer")
+		// todo
+	} else if currentToken == "string" {
+		fmt.Print("string")
+		// todo
+	} else if currentToken == "real" {
+		fmt.Print("real")
+		// todo
+	} else if currentToken == "boolean" {
+		fmt.Print("boolean")
+		// todo
+	} else if currentToken == "char" {
+		fmt.Print("char")
+		// todo
+	} else {
+		// todo Identifier
 	}
 }
